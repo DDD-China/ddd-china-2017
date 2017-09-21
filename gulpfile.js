8fc30898c2175aaa835b06cd1f995dbe89bf51ab
stringlc <whulc@outlook.com>
2017-09-21 10:58:37 +0800

    [Cong] add new page for new design.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
-var fileinclude  = require('gulp-file-include');
+var fileinclude = require('gulp-file-include');
 
-gulp.task('default', function() {
+gulp.task('watch', function () {
+    gulp.watch(['includes/*.html', 'src/*.html'], ['default']);
+});
+
+gulp.task('default', function () {
     gulp.src('src/**.html')
         .pipe(fileinclude({
             prefix: '@@',
---
+++ webContent/js/map.js
@@ -0,0 +1,37 @@
+$(function init() {
+    var map = new AMap.Map('container', {
+        center: [116.432550, 39.939036],
+        zoom: 17
+    });
+    map.plugin(["AMap.ToolBar"], function () {
+        map.addControl(new AMap.ToolBar());
+    });
+    var marker = new AMap.Marker({
+        map: map,
+        position: [116.432550, 39.939036]
+    });
+    marker.setLabel({
+        offset: new AMap.Pixel(20, 20),//修改label相对于maker的位置
+        content: "点击Marker打开高德地图"
+    });
+    marker.on('click', function (e) {
+        marker.markOnAMAP({
+            name: '首开广场',
+            position: marker.getPosition()
+        })
+    })
+});
+
+function myPosition() {
+    map.plugin('AMap.Geolocation', function () {
+        geolocation = new AMap.Geolocation({
+            enableHighAccuracy: true,//是否使用高精度定位，默认:true
+            timeout: 10000,          //超过10秒后停止定位，默认：无穷大
+            buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+            zoomToAccuracy: true,      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+            buttonPosition: 'RB'
+        });
+        map.addControl(geolocation);
+        geolocation.getCurrentPosition();
+    });
+}
