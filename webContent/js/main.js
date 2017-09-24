4f95c2cf8bdd58e836536086adb2929ae16312a7
stringlc <whulc@outlook.com>
2017-09-24 21:52:39 +0800

    [Cong] Fix navbar to only changing color after click on link and changed the footer text.

--- webContent/js/main.js
+++ webContent/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
-    $('.navbar .menu-item').on('click', function () {
+    $('.navbar .menu-item>a').on('click', function () {
         $('.navbar .menu-item').removeClass('menu-item-selected');
-        $(this).addClass('menu-item-selected');
+        $(this).parent().addClass('menu-item-selected');
     })
 })
