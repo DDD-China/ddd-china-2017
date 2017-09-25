c3b38ca446a3fc0ceb9f2bb8d86a3996deb90457
stringlc <whulc@outlook.com>
2017-09-25 14:23:27 +0800

    [Cong] finish speaker info-detail page.

--- webContent/js/speakers.js
+++ webContent/js/speakers.js
@@ -1,5 +1,6 @@
-$(
-    function () {
-    JSON.parse()
-    }
-)
+$(function () {
+    $('.speaker-info .speaker-img img').attr('src', speaker['speaker-img']).attr('alt', speaker['speaker-name']);
+    $('.speaker-info .speaker-detail .speaker-detail-introduction .content').append(speaker['speaker-introduction']);
+    $('.speaker-info .speaker-detail .speaker-detail-topic .content').append(speaker['speaker-topic']);
+    $('.speaker-info .speaker-detail .speaker-detail-outline .content').append(speaker['speaker-outline']);
+})
---
+++ webContent/js/speakers/speaker-test.js
@@ -0,0 +1,8 @@
+var speaker = {
+    'speaker-img': '/resource/speakers/speaker-test.jpg',
+    'speaker-name': '熊本熊',
+    'speaker-introduction': '熊本熊，卡通形象，这是一个样例。',
+    'speaker-topic': 'speaker-test will introduce new techs on Web development',
+    'speaker-outline': '<p>part 1: Web framework</p><p>part 2: Web usage</p>'
+
+}
