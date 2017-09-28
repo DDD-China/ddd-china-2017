fb8369afbf6765a436f24514b2c938870d1a35ba
stringlc <whulc@outlook.com>
2017-09-29 02:07:11 +0800

    [Cong] Finish gulp watch setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
     gulp_sequence = require('gulp-sequence'),
+    rename = require('gulp-rename'),
     fileinclude = require('gulp-file-include'),
-    minifyjs = require('gulp-js-minify');
+    minifyjs = require('gulp-js-minify'),
+    less = require('gulp-less');
 
-gulp.task('watch', gulp_sequence(['watch_html', 'watch_js']));
+gulp.task('watch', gulp_sequence(['watch_html', 'watch_js', 'watch_css']));
 
 
 gulp.task('watch_html', function () {
@@ -14,6 +16,9 @@ gulp.task('watch_js', function () {
     gulp.watch(['webContent/js/*.js', 'webContent/js/speakers/*.js'], ['minify-js-index', 'minify-js-speakers']);
 });
 
+gulp.task('watch_css', function () {
+    gulp.watch('webContent/css/*.less', ['compile-less']);
+})
 
 gulp.task('include_index', function () {
     gulp.src('src/**.html')
@@ -34,13 +39,21 @@ gulp.task('include_speakers', function () {
 });
 
 gulp.task('minify-js-index', function () {
-    gulp.src('webContent/js/*.js')
+    gulp.src(['webContent/js/*.js', '!webContent/js/*.min.js'])
         .pipe(minifyjs())
-        .pipe(gulp.dest('webContent/js/lib/'));
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('webContent/js/lib'));
 });
 
 gulp.task('minify-js-speakers', function () {
-    gulp.src('webContent/js/speakers/*.js')
+    gulp.src(['webContent/js/speakers/*.js', '!webContent/js/speakers/*.min.js'])
         .pipe(minifyjs())
+		    .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('webContent/js/lib/'));
 });
+
+gulp.task('compile-less', function () {
+    gulp.src('webContent/css/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('webContent/css/'))
+})
