51e5471fff3c5b6f9d88dbb6ce22fa6756924db9
stringlc <whulc@outlook.com>
2017-09-29 00:31:54 +0800

    [Cong] update gulpfile for automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
-var gulp = require('gulp');
-var fileinclude = require('gulp-file-include');
+var gulp = require('gulp'),
+    gulp_watch = require('gulp-watch'),
+    gulp_sequence = require('gulp-sequence'),
+    fileinclude = require('gulp-file-include'),
+    minifyjs = require('gulp-js-minify');
 
-gulp.task('watch', function () {
-    gulp.watch(['includes/*.html', 'src/*.html', 'src/speakers/*.html'], ['default', 'speakers']);
+gulp.task('watch', gulp_sequence(['watch_html', 'watch_js']));
+
+
+gulp.task('watch_html', function () {
+    gulp_watch(['includes/*.html', 'src/*.html', 'src/speakers/*.html'], ['include_index', 'include_speakers']);
+})
+
+gulp.task('watch_js', function () {
+    gulp_watch(['webContent/js/*.js', 'webContent/js/speakers/*.js'], ['minify-js-index', 'minify-js-speakers']);
 });
 
-gulp.task('default', function () {
+
+gulp.task('include_index', function () {
     gulp.src('src/**.html')
         .pipe(fileinclude({
             prefix: '@@',
@@ -14,11 +25,23 @@ gulp.task('default', function () {
         .pipe(gulp.dest('webContent'));
 });
 
-gulp.task('speakers', function () {
+gulp.task('include_speakers', function () {
     gulp.src('src/speakers/**.html')
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
         .pipe(gulp.dest('webContent/speakers/'));
-})
+});
+
+gulp.task('minify-js-index', function () {
+    gulp.src('webContent/js/*.js')
+        .pipe(minifyjs())
+        .pipe(gulp.dest('webContent/js/'));
+});
+
+gulp.task('minify-js-speakers', function () {
+    gulp.src('webContent/js/speakers/*.js')
+        .pipe(minifyjs())
+        .pipe(gulp.dest('webContent/js/speakers/'));
+});
