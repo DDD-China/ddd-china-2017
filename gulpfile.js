33929fc68dbddc12a367a3d09d67ed790bbaccc7
stringlc <whulc@outlook.com>
2017-09-29 01:05:49 +0800

    [Cong] Add npm and gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    gulp_watch = require('gulp-watch'),
     gulp_sequence = require('gulp-sequence'),
     fileinclude = require('gulp-file-include'),
     minifyjs = require('gulp-js-minify');
@@ -8,11 +7,11 @@ gulp.task('watch', gulp_sequence(['watch_html', 'watch_js']));
 
 
 gulp.task('watch_html', function () {
-    gulp_watch(['includes/*.html', 'src/*.html', 'src/speakers/*.html'], ['include_index', 'include_speakers']);
+    gulp.watch(['includes/*.html', 'src/*.html', 'src/speakers/*.html'], ['include_index', 'include_speakers']);
 })
 
 gulp.task('watch_js', function () {
-    gulp_watch(['webContent/js/*.js', 'webContent/js/speakers/*.js'], ['minify-js-index', 'minify-js-speakers']);
+    gulp.watch(['webContent/js/*.js', 'webContent/js/speakers/*.js'], ['minify-js-index', 'minify-js-speakers']);
 });
 
 
@@ -37,11 +36,11 @@ gulp.task('include_speakers', function () {
 gulp.task('minify-js-index', function () {
     gulp.src('webContent/js/*.js')
         .pipe(minifyjs())
-        .pipe(gulp.dest('webContent/js/'));
+        .pipe(gulp.dest('webContent/js/lib/'));
 });
 
 gulp.task('minify-js-speakers', function () {
     gulp.src('webContent/js/speakers/*.js')
         .pipe(minifyjs())
-        .pipe(gulp.dest('webContent/js/speakers/'));
+        .pipe(gulp.dest('webContent/js/lib/'));
 });
