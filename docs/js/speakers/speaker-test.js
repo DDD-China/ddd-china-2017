1d97fedd4635fd52beaa64971d4eb416ccb06f72
Zhengyang Zhao <zyzhao@thoughtworks.com>
2017-09-30 16:36:48 +0800

    fix image

--- docs/js/speakers/speaker-test.js
+++ docs/js/speakers/speaker-test.js
@@ -1,8 +1,7 @@
 var speaker = {
-    'speaker-img': '../resource/speakers/speaker-test.jpg',
-    'speaker-name': '熊本熊',
-    'speaker-introduction': '熊本熊，卡通形象，这是一个样例。',
+    'speaker-img': '../resource/speakers/alberto-brandolini.jpg',
+    'speaker-name': 'alberto-brandolini',
+    'speaker-introduction': '全球软件巨匠',
     'speaker-topic': 'speaker-test will introduce new techs on Web development',
     'speaker-outline': '<p>part 1: Web framework</p><p>part 2: Web usage</p>'
-
 }
