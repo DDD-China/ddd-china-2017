5d3622268eee9d367d174011b4c26fd46d7615f0
Zhengyang Zhao <zyzhao@thoughtworks.com>
2017-10-01 15:44:32 +0800

    Add speakers page with right content and url, and still need to fix the absolute route later.

--- docs/js/speakers.js
+++ docs/js/speakers.js
@@ -1,6 +1,8 @@
 $(function () {
-    $('.speaker-info .speaker-img img').attr('src', speaker['speaker-img']).attr('alt', speaker['speaker-name']);
-    $('.speaker-info .speaker-detail .speaker-detail-introduction .content').append(speaker['speaker-introduction']);
-    $('.speaker-info .speaker-detail .speaker-detail-topic .content').append(speaker['speaker-topic']);
-    $('.speaker-info .speaker-detail .speaker-detail-outline .content').append(speaker['speaker-outline']);
+    var id = Number(window.location.search.split('=')[1]) - 1
+    console.log(id)
+    $('.speaker-info .speaker-img img').attr('src', speakers[id]['speaker-img']).attr('alt', speakers[id]['speaker-name']);
+    $('.speaker-info .speaker-detail .speaker-detail-introduction .content').append(speakers[id]['speaker-introduction']);
+    $('.speaker-info .speaker-detail .speaker-detail-topic .content').append(speakers[id]['speaker-topic']);
+    $('.speaker-info .speaker-detail .speaker-detail-outline .content').append(speakers[id]['speaker-outline']);
 })
--- docs/js/speakers/speaker-test.js
+++ docs/js/speakers/speaker-test.js
@@ -1,7 +1,19 @@
-var speaker = {
+var speakers = [{
     'speaker-img': '../resource/speakers/alberto-brandolini.jpg',
-    'speaker-name': 'alberto-brandolini',
+    'speaker-name': 'Alberto Brandolini',
     'speaker-introduction': '全球软件巨匠',
     'speaker-topic': 'speaker-test will introduce new techs on Web development',
     'speaker-outline': '<p>part 1: Web framework</p><p>part 2: Web usage</p>'
-}
+}, {
+    'speaker-img': '../resource/speakers/chrishead_large_cropped.jpg',
+    'speaker-name': 'Chris Richardson',
+    'speaker-introduction': '世界著名软件大师',
+    'speaker-topic': 'speaker-test will introduce new techs on Web development',
+    'speaker-outline': '<p>part 1: Web framework</p><p>part 2: Web usage</p>'
+}, {
+    'speaker-img': '../resource/speakers/xiaoran.jpg',
+    'speaker-name': '肖然',
+    'speaker-introduction': 'ThoughtWorks 咨询总监',
+    'speaker-topic': 'speaker-test will introduce new techs on Web development',
+    'speaker-outline': '<p>part 1: Web framework</p><p>part 2: Web usage</p>'
+}]
