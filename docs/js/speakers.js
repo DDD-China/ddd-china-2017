e89aa6e67b4fed2ceeed84417a57fd32a6f7071b
Zhengyang Zhao <zyzhao@thoughtworks.com>
2017-10-07 20:26:09 +0800

    Add new 4 speakers and delete one.

--- docs/js/speakers.js
+++ docs/js/speakers.js
@@ -2,7 +2,7 @@ $(function () {
     var id = Number(window.location.search.split('=')[1]) - 1
     console.log(id)
     $('.speaker-info .speaker-img img').attr('src', speakers[id]['speaker-img']).attr('alt', speakers[id]['speaker-name']);
-    $('.speaker-info .speaker-detail .speaker-detail-introduction .content').append(speakers[id]['speaker-introduction']);
+    $('.speaker-info .speaker-detail .speaker-detail-introduction .content').append(speakers[id]['speaker-information']);
     $('.speaker-info .speaker-detail .speaker-detail-topic .content').append(speakers[id]['speaker-topic']);
     $('.speaker-info .speaker-detail .speaker-detail-outline .content').append(speakers[id]['speaker-outline']);
 })
--- docs/js/speakers/speaker-test.js
+++ docs/js/speakers/speaker-test.js
@@ -2,18 +2,115 @@ var speakers = [{
     'speaker-img': '../resource/speakers/alberto-brandolini.jpg',
     'speaker-name': 'Alberto Brandolini',
     'speaker-introduction': '全球软件巨匠',
-    'speaker-topic': 'speaker-test will introduce new techs on Web development',
-    'speaker-outline': '<p>part 1: Web framework</p><p>part 2: Web usage</p>'
-}, {
-    'speaker-img': '../resource/speakers/chrishead_large_cropped.jpg',
-    'speaker-name': 'Chris Richardson',
-    'speaker-introduction': '世界著名软件大师',
-    'speaker-topic': 'speaker-test will introduce new techs on Web development',
-    'speaker-outline': '<p>part 1: Web framework</p><p>part 2: Web usage</p>'
+    'speaker-information': '<p>全球软件巨匠</p><p>Event Storming 之父</p>',
+    'speaker-topic': '峰会 keynote 演讲',
+    'speaker-outline': ''
 }, {
     'speaker-img': '../resource/speakers/xiaoran.jpg',
     'speaker-name': '肖然',
-    'speaker-introduction': 'ThoughtWorks 咨询总监',
-    'speaker-topic': 'speaker-test will introduce new techs on Web development',
-    'speaker-outline': '<p>part 1: Web framework</p><p>part 2: Web usage</p>'
+    'speaker-introduction': '精益方法布道师',
+    'speaker-information': `
+        肖然<br>
+        ThoughtWorks咨询及设计总监，国内最早的敏捷精益方法实践者，适应性领导力及领
+        域驱动架构方法首席讲师及教练。作为咨询顾问长期辅导国内外知名企业进行精益企
+        业的建设，近期在多个大型开发团队推广领域驱动设计方法及微服务架构。
+    `,
+    'speaker-topic': `
+        为不确定性架构（Keynote演讲）<br>
+        - 第四次工业革命引爆了一个科技驱动的新时代，软件成为了这个时代的核心竞
+        争力。然而在行业光环下的软件工程人员们日子并不好过，高速乃至颠覆性的
+        发展给我们制造了大量的不确定性，迫使我们去思考新的技术架构方式来适应
+        这个时代。正是在这样的背景下，领域驱动设计的方式再次进入了我们的视野
+        ，成为了我们响应不确定性的重要方法。
+    `,
+    'speaker-outline': `
+        不确定性的悖论<br>
+        架构设计的达摩克利斯之剑<br>
+        面向不确定性演进<br>
+        微服务架构红眼病
+    `,
+}, {
+    'speaker-img': '../resource/speakers/zhangyi.png',
+    'speaker-name': '张逸',
+    'speaker-introduction': '编码实践、加固设计、领域驱动实践领先者',
+    'speaker-information': `
+        张逸<br>
+        致力于编码实践、架构设计、领域驱动、大数据分析，并在金融、制造业、医疗、通
+        信、企业架构、互联网诸多行业有丰富的开发与设计经验。
+    `,
+    'speaker-topic': `
+        Bounded Context的实践意义（领域驱动设计与演进式架构及微服务专题演讲）<br>
+        Bounded Context作为DDD战略设计的重要入口与概念，可以与微服务设计有机地结
+        合起来。本演讲会深入剖析Bounded Context的本质原理，提供切实可行的方法帮助
+        团队成员发现Bounded Context，探讨Bounded Context的边界，并结合Event
+        Sourcing、微服务架构、六边形架构模式、用户故事地图，总结出从架构到编码实现
+        的实践意义。
+    `,
+    'speaker-outline': `
+        Bounded Context的本质意义<br>
+        如何发现合理的Bounded Context Bounded Context的边界<br>
+        Bounded Context之间的通信<br>
+        从战略架构到战术实现
+    `,
+}, {
+    'speaker-img': '../resource/speakers/lixin.png',
+    'speaker-name': '李新',
+    'speaker-introduction': '骨灰级程序员',
+    'speaker-information': `
+        李新<br>
+        代码20+年，精通从C++到Golang，用Java实现过互联网大型分布是系统，用Scala设
+        计过现代通信网络。像计算机一样思考，追求代码架构设计的大道至简。
+    `,
+    'speaker-topic': `
+        （领域驱动设计与组织治理专题演讲）
+    `,
+    'speaker-outline': `
+        待更新
+    `,
+}, {
+    'speaker-img': '../resource/speakers/tengyun.png',
+    'speaker-name': '滕云',
+    'speaker-introduction': '实现领域驱动设计译者',
+    'speaker-information': `
+        滕云<br>
+        ThoughtWorks咨询师，6年+一线编码经验，3年+领域驱动设计实践经验，技术专研
+        领域包括DDD、Spring生态圈、DevOps、Linux以及软件匠艺；译有《实现领域驱动设
+        计》和《人件》等书。
+    `,
+    'speaker-topic': `
+        DDD（领域驱动设计）——没那么难（领域驱动设计与企业架构演讲）<br>
+        DDD社区一直不乏思想领袖，但是要将DDD落地确是另一回事情。本次演讲主要面向
+        希望在项目上实际实施DDD的听众，包含DDD中的战略设计和战术设计，也包含DDD
+        与微服务的关系分析。
+    `,
+    'speaker-outline': `
+        <ol>
+            <li>DDD限界上下文划分</li>
+            <li>DDD与微服务 </li>
+            <li>聚合根识别 </li>
+            <li>实体和值对象 </li>
+            <li>领域服务 </li>
+            <li>六边形架构</li>
+        </ol>
+    `,
+}, {
+    'speaker-img': '../resource/speakers/kouyu.png',
+    'speaker-name': '寇宇',
+    'speaker-introduction': '微服务架构、系统监控、企业架构规划专家',
+    'speaker-information': `
+        寇宇<br>
+        融数数据研发总监，专注于微服务架构、系统监控、企业架构规划等领域。曾就职与IBM、苏宁易购、神州数码等企业。
+    `,
+    'speaker-topic': `
+        微服务架构理念，如何利用领域驱动设计对传统单体架构或者SOA架构的系统进行微服务架构改造，进而构建微服务架构体系；<br>
+        介绍微服务治理方案和微服务架构下的系统监控方法。
+    `,
+    'speaker-outline': `
+        <ol>
+            <li>微服务架构及其特点</li>
+            <li>微服务划定和抽象</li>
+            <li>微服务架构选型</li>
+            <li>微服务治理与监控</li>
+        </ol>
+    `,
 }]
