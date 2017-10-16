a6b73c45ae9c6427f6502afef379a7a6ea59f049
Congliu <whulc@outlook.com>
2017-10-16 19:57:17 +0800

    [Cong] Change information about speakers.

--- docs/js/speakers/speaker-test.js
+++ docs/js/speakers/speaker-test.js
@@ -2,65 +2,68 @@ var speakers = [{
     'speaker-img': '../resource/speakers/alberto-brandolini.jpg',
     'speaker-name': 'Alberto Brandolini',
     'speaker-introduction': '全球软件巨匠',
-    'speaker-information': '<p>全球软件巨匠</p><p>Event Storming 之父</p>',
-    'speaker-topic': '峰会 keynote 演讲',
+    'speaker-information': '' +
+    '<span class="bold-text">Alberto Brandolini</span><br>' +
+    '<p>全球软件巨匠，Event Storming 之父</p>',
+    'speaker-topic': '<span class="bold-text">「why ddd matters today」</span><br>',
     'speaker-outline': ''
 }, {
     'speaker-img': '../resource/speakers/xiaoran.jpg',
     'speaker-name': '肖然',
     'speaker-introduction': '精益方法布道师',
     'speaker-information': `
-        肖然<br>
-        ThoughtWorks咨询及设计总监，国内最早的敏捷精益方法实践者，适应性领导力及领
-        域驱动架构方法首席讲师及教练。作为咨询顾问长期辅导国内外知名企业进行精益企
-        业的建设，近期在多个大型开发团队推广领域驱动设计方法及微服务架构。
+        <span class="bold-text">肖然</span><br>
+        ThoughtWorks咨询和设计总监，精益方法布道师<br>
+        国内最早的敏捷精益方法实践者，适应性领导力及领域驱动架构方法首席讲师及教练。
+        作为咨询顾问长期辅导国内外知名企业进行精益企业的建设，近期在多个大型开发团
+        队推广领域驱动设计方法及微服务架构。
     `,
     'speaker-topic': `
-        为不确定性架构（Keynote演讲）<br>
-        - 第四次工业革命引爆了一个科技驱动的新时代，软件成为了这个时代的核心竞
-        争力。然而在行业光环下的软件工程人员们日子并不好过，高速乃至颠覆性的
-        发展给我们制造了大量的不确定性，迫使我们去思考新的技术架构方式来适应
-        这个时代。正是在这样的背景下，领域驱动设计的方式再次进入了我们的视野
-        ，成为了我们响应不确定性的重要方法。
+        <span class="bold-text">「为不确定性架构」</span><br>
+        第四次工业革命引爆了一个科技驱动的新时代，软件成为了这个时代的核心竞争力。
+        然而在行业光环下的软件工程人员们日子并不好过，高速乃至颠覆性的发展给我们制造了大量的不确定性，
+        迫使我们去思考新的技术架构方式来适应这个时代。正是在这样的背景下，领域驱动设计的方式再次进入
+        了我们的视野，成为了我们响应不确定性的重要方法。
     `,
     'speaker-outline': `
         <ul>
-        <li>不确定性的悖论</li>
-        <li>架构设计的达摩克利斯之剑</li>
-        <li>面向不确定性演进</li>
-        <li>微服务架构红眼病</li>
-</ul>
-
+            <li>不确定性的悖论</li>
+            <li>架构设计的达摩克利斯之剑</li>
+            <li>面向不确定性演进</li>
+            <li>微服务架构红眼病</li>
+        </ul>
     `,
 }, {
     'speaker-img': '../resource/speakers/zhangyi.png',
     'speaker-name': '张逸',
     'speaker-introduction': '编码实践、加固设计、领域驱动实践领先者',
     'speaker-information': `
-        张逸<br>
-        致力于编码实践、架构设计、领域驱动、大数据分析，并在金融、制造业、医疗、通
-        信、企业架构、互联网诸多行业有丰富的开发与设计经验。
+        <span class="bold-text">张逸</span><br>
+        成创翻有限公司CEO，编码实践、加固设计、领域驱动实践领先者<br>
+        致力于编码实践、架构设计、领域驱动、大数据分析，并在金融、
+        制造业、医疗、通信、企业架构、互联网诸多行业有丰富的开发与设计经验。
     `,
     'speaker-topic': `
-        Bounded Context的实践意义（领域驱动设计与演进式架构及微服务专题演讲）<br>
-        Bounded Context作为DDD战略设计的重要入口与概念，可以与微服务设计有机地结
-        合起来。<br><br>本演讲会深入剖析Bounded Context的本质原理，提供切实可行的方法帮助
-        团队成员发现Bounded Context，探讨Bounded Context的边界，并结合Event
-        Sourcing、微服务架构、六边形架构模式、用户故事地图，总结出从架构到编码实现
-        的实践意义。
+        <span class="bold-text">「Bounded Context的实践意义」</span><br>
+        Bounded Context作为DDD战略设计的重要入口与概念，可以与微服务设计有机地结合起来。<br>
+        本演讲会深入剖析Bounded Context的本质原理，提供切实可行的方法帮助团队成员发现
+        Bounded Context，探讨Bounded Context的边界，并结合Event Sourcing、微服务架构、
+        六边形架构模式、用户故事地图，总结出从架构到编码实现的实践意义。
     `,
     'speaker-outline': `
-        Bounded Context的本质意义<br>
-        如何发现合理的Bounded Context Bounded Context的边界<br>
-        Bounded Context之间的通信<br>
-        从战略架构到战术实现
+        <ul>
+            <li>Bounded Context的本质意义</li>
+            <li>如何发现合理的Bounded Context Bounded Context的边界</li>
+            <li>Bounded Context之间的通信</li>
+            <li>从战略架构到战术实现</li>
+        </ul>
     `,
 }, {
     'speaker-img': '../resource/speakers/lixin.png',
     'speaker-name': '李新',
     'speaker-introduction': '骨灰级程序员',
     'speaker-information': `
-        李新<br>
+        <span class="bold-text">李新</span><br>
         代码20+年，精通从C++到Golang，用Java实现过互联网大型分布式系统，用Scala设
         计过现代通信网络。像计算机一样思考，追求代码架构设计的大道至简。
     `,
@@ -75,85 +78,103 @@ var speakers = [{
     'speaker-name': '滕云',
     'speaker-introduction': '实现领域驱动设计译者',
     'speaker-information': `
-        滕云<br>
-        ThoughtWorks咨询师，6年+一线编码经验，3年+领域驱动设计实践经验，技术专研
-        领域包括DDD、Spring生态圈、DevOps、Linux以及软件匠艺；译有《实现领域驱动设
-        计》和《人件》等书。
+        <span class="bold-text">腾云</span><br>
+        《实现领域驱动设计》译者<br>
+        ThoughtWorks咨询师，6年+一线编码经验，3年+领域驱动设计实践经验，
+        技术专研领域包括DDD、Spring生态圈、DevOps、Linux以及软件匠艺；
+        译有《实现领域驱动设计》和《人件》等书。
+
     `,
     'speaker-topic': `
-        DDD（领域驱动设计）——没那么难（领域驱动设计与企业架构演讲）<br>
-        DDD社区一直不乏思想领袖，但是要将DDD落地确是另一回事情。本次演讲主要面向
-        希望在项目上实际实施DDD的听众，包含DDD中的战略设计和战术设计，也包含DDD
-        与微服务的关系分析。
+        <span class="bold-text">「DDD——没那么难」</span><br>
+        DDD社区一直不乏思想领袖，但是要将DDD落地确是另一回事情。
+        本次演讲主要面向希望在项目上实际实施DDD的听众，包含DDD中
+        的战略设计和战术设计，也包含DDD与微服务的关系分析。
     `,
     'speaker-outline': `
-        <ol>
+        <ul>
             <li>DDD限界上下文划分</li>
             <li>DDD与微服务 </li>
             <li>聚合根识别 </li>
             <li>实体和值对象 </li>
             <li>领域服务 </li>
             <li>六边形架构</li>
-        </ol>
+        </ul>
     `,
 }, {
     'speaker-img': '../resource/speakers/kouyu.png',
     'speaker-name': '寇宇',
     'speaker-introduction': '微服务架构、系统监控、企业架构规划专家',
     'speaker-information': `
-        寇宇<br>
+        <span class="bold-text">寇宇</span><br>
+        融数数据研发总监，微服务架构、系统监控、企业架构规划专家<br>
         融数数据研发总监，专注于微服务架构、系统监控、企业架构规划等领域。曾就职与IBM、苏宁易购、神州数码等企业。
+
     `,
     'speaker-topic': `
-        微服务架构理念，如何利用领域驱动设计对传统单体架构或者SOA架构的系统进行微服务架构改造，进而构建微服务架构体系；<br>
+        <span class="bold-text">「微服务治理及监控」</span><br>
+        微服务架构理念，如何利用领域驱动设计对传统单体架构或者SOA架构的系统进行微服务架构改造，进而构建微服务架构体系；
         介绍微服务治理方案和微服务架构下的系统监控方法。
+
     `,
     'speaker-outline': `
-        <ol>
+        <ul>
             <li>微服务架构及其特点</li>
             <li>微服务划定和抽象</li>
             <li>微服务架构选型</li>
             <li>微服务治理与监控</li>
-        </ol>
+        </ul>
     `,
 }, {
     'speaker-img': '../resource/speakers/zhangqunhui.png',
     'speaker-name': '张群辉',
     'speaker-introduction': '阿里盒马架构总监<br>领域驱动设计国内最早的一批实践者',
-    'speaker-information': '张群辉<br>15+年技术及管理实战经验，前阿里基础机构事业部工程效率总监，' +
-    '长期一线指导大型复杂系统的架构设计，DevOps、微服务架构及领域驱动设计国内最早的一批实践者。' +
-    '崇尚实践出真知，一直奋斗在技术一线。',
-    'speaker-topic': '盒马作为阿里巴巴新零售的排头兵，第一次真正意义上涉入零售行业，创建中国新零售模式下的供应链体系，' +
-    '颠覆传统ERP是盒马技术最近几年不停探索的重点。<br>互联网+传统零售业务复杂，领域模型在这个复杂的体' +
-    '系里变得尤为重要，有别于传统互联网电商架构，本演讲将会深入浅出阐述领域驱动开发在盒马模式下如何演化，' +
-    '落地，如何在互联网架构下完整实施具有阿里特点的领域驱动设计模式',
-    'speaker-outline': '<ol>' +
+    'speaker-information': '<span class="bold-text">张群辉</span><br>' +
+    '阿里盒马架构总监，领域驱动设计国内最早的一批实践者<br>' +
+    '15 + 年技术及管理实战经验，前阿里基础机构事业部工程效率总监，' +
+    '长期一线指导大型复杂系统的架构设计，DevOps、微服务架构及领域驱动' +
+    '设计国内最早的一批实践者。崇尚实践出真知，一直奋斗在技术一线。'
+    ,
+    'speaker-topic':
+    '<span class="bold-text">「DDD@盒马」</span><br>' +
+    '盒马作为阿里巴巴新零售的排头兵，第一次真正意义上涉入零售行业，' +
+    '创建中国新零售模式下的供应链体系，颠覆传统ERP是盒马技术最近几年不停探索的重点。' +
+    '互联网 + 传统零售业务复杂，领域模型在这个复杂的体系里变得尤为重要，' +
+    '有别于传统互联网电商架构，本演讲将会深入浅出阐述领域驱动开发在盒马模式下如何演化，' +
+    '落地，如何在互联网架构下完整实施具有阿里特点的领域驱动设计模式'
+    ,
+    'speaker-outline':
+    '<ul>' +
     '<li>领域模型：基于对象 vs 基于数据库</li>' +
     '<li>领域模型下的依赖注入</li>' +
     '<li>repository的实现方式</li>' +
-    '<li>领域模型下的部署架构</li></ol>'
-}, {
-    'speaker-img': '../resource/speakers/wangbo.png',
-    'speaker-name': '王博',
-    'speaker-introduction': '资深DDD实践者',
-    'speaker-information': `
-        王博<br>
-        独立咨询顾问，专注于大型系统软件的领域建模设计、重构及服务化架构设计等领域。曾先后在电信、云服务、
-        自动驾驶等领域帮助客户进行微服务架构和领域驱动设计等工作，参与并指导过大规模的代码重构和架构落地等工作。
-    `,
-    'speaker-topic': `
-        做好领域驱动设计是有门槛的。作为一名软件技术咨询师，曾在不同行业、不同技术领域进行软件设计和开发工作，
-        遇见过各种DDD在实施中的问题和困难。本议题将这些常见问题与困难进行整理，结合案例给出解决建议，帮助大家更好的使用DDD解决复杂问题。
-    `,
-    'speaker-outline': `
-        DDD技术发展简介<br>
-        支撑DDD的软件开发方法和技能<br>
-        复杂软件开发中使用DDD常见的问题与困难<br>
-        <ul>
-            <li>案例1：缺乏演进式设计能力的领域驱动设计</li>
-            <li>案例2：贫血的领域驱动设计</li>
-            <li>案例3：抽象不足的领域模型</li>
-            <li>案例4：面向底层的、高性能软件的领域建模</li>
-        </ul>
-    `,
-}]
+    '<li>领域模型下的部署架构</li>' +
+    '</ul>'
+},
+    {
+        'speaker-img':
+            '../resource/speakers/wangbo.png',
+        'speaker-name':
+            '王博',
+        'speaker-introduction':
+            '资深DDD实践者',
+        'speaker-information':
+        '<span class="bold-text">王博</span><br>' +
+        '资深程序员、软件工匠<br>' +
+        '独立咨询顾问，专注于大型系统软件的领域建模设计、重构及服务化架构设计等领域。' +
+        '曾先后在电信、云服务、自动驾驶等领域帮助客户进行微服务架构和领域驱动设计等工作，' +
+        '参与并指导过大规模的代码重构和架构落地等工作。',
+        'speaker-topic': '<span class = "bold-text">「DDD常见困难及解决之道」</span><br>' +
+        '做好领域驱动设计是有门槛的。作为一名软件技术咨询师，曾在不同行业、' +
+        '不同技术领域进行软件设计和开发工作，遇见过各种DDD在实施中的问题和困难。' +
+        '本议题将这些常见问题与困难进行整理，结合案例给出解决建议，帮助大家更好的' +
+        '使用DDD解决复杂问题。',
+        'speaker-outline': 'DDD技术发展简介<br>支撑DDD的软件开发方法和技能<br>复杂软件开发中使用DDD常见的问题与困难<br>' +
+        '<ul>' +
+        '<li> 案例1：缺乏演进式设计能力的领域驱动设计 </li>' +
+        '<li> 案例2：贫血的领域驱动设计 </li>' +
+        '<li> 案例3：抽象不足的领域模型 </li>' +
+        '<li> 案例4：面向底层的、高性能软件的领域建模 </li>' +
+        '</ul>',
+    }
+]
